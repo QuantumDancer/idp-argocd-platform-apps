eeb202aeb249fc6bbade2e0fa5612b0a1452f213
Benjamin Rottler <benjamin@dierottlers.de>
2025-11-10 21:51:29 +0100

    Add boostrapping of repo and root project

---
+++ README.md
@@ -0,0 +1,9 @@
+# Platform Applications
+
+## Bootstrapping
+
+- Create a GitLab Deploy Token for this repo: https://docs.gitlab.com/user/project/deploy_tokens/#create-a-deploy-token
+- Create `./bootstrap/repo.yaml` (use `./bootstrap/repo.yaml.example` as template)
+- Run `./bootstrap/bootstrap.sh`
+
+Follow the printed instructions how to set up port forwarding and how to retrieve the admin secret.
--- bootstrap/bootstrap.sh
+++ bootstrap/bootstrap.sh
@@ -5,5 +5,9 @@ helm repo add argo https://argoproj.github.io/argo-helm
 helm upgrade --install argocd argo/argo-cd --version 9.1.0 -n argocd --create-namespace -f bootstrap/argocd-values.yaml
 echo ""
 
+echo "Applying bootstrap configuration"
+kubectl apply -f bootstrap/repo.yaml
+kubectl apply -f bootstrap/root-app.yaml
+
 echo "Run this command to enable port forwarding:"
 echo "kubectl port-forward svc/argocd-server -n argocd 8080:443"
