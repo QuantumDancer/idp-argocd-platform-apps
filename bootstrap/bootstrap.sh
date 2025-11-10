875689bb45bb4b3e2cdad5fd8faa8294e96d9b89
Benjamin Rottler <benjamin@dierottlers.de>
2025-11-10 21:41:47 +0100

    Initial commit

---
+++ bootstrap/bootstrap.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+echo "Installing ArgoCD..."
+helm repo add argo https://argoproj.github.io/argo-helm
+helm upgrade --install argocd argo/argo-cd --version 9.1.0 -n argocd --create-namespace -f bootstrap/argocd-values.yaml
+echo ""
+
+echo "Run this command to enable port forwarding:"
+echo "kubectl port-forward svc/argocd-server -n argocd 8080:443"
